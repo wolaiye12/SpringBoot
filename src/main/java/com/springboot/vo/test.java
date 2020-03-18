936d7a700fba1c853deb5fa5810fdeebf2a63673
王晓宁 <ning1528451011@163.com>
2020-03-18 09:43:07 +0800

    第er次

--- src/main/java/com/springboot/vo/test.java
+++ src/main/java/com/springboot/vo/test.java
@@ -4,6 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("wxn");
         System.out.println("wxn");
-        System.out.println("12122");
+        System.out.println("2222");
     }
 }
