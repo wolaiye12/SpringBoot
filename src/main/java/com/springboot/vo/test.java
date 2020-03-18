1496bd676bc8b7badf4ac59662085991f3a696ab
王晓宁 <ning1528451011@163.com>
2020-03-18 09:41:26 +0800

    第一次

--- src/main/java/com/springboot/vo/test.java
+++ src/main/java/com/springboot/vo/test.java
@@ -3,11 +3,9 @@ package com.springboot.vo;
 public class test {
     public static void main(String[] args) {
         System.out.println("wxn");
-        System.out.println("111");
-        System.out.println("111");
-        System.out.println("111");
-        System.out.println("111");
-        System.out.println("111");
-
+        System.out.println("wxn");
+        System.out.println("wxn");
+        System.out.println("wxn");
+        System.out.println("wxn");
     }
 }
